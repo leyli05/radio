a294e568cd3c1b7573628ada31e9761a64d45577
leily <leily@example.com>
2023-11-14 16:56:54 +0300

    Revert "Third commit"
    This reverts commit fc2914c05ce8d794f4e7c4c21f05725408543e67.

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -2,104 +2,62 @@ package ru.netology.radio;
 
 public class Radio {
 
-        private int currentVolume;
-        private int currentRadioStation = 10;
-        private int maxRadioStation = 10;
-        private int minRadioStation = 0;
-        private int maxVolume = 100;
-        private int minVolume = 0;
 
+    private int currentVolume;
+    private int currentRadioStation;
 
-        public Radio() {
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
 
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int newCurrentRadioStation) {
+        if (newCurrentRadioStation > 9) {
+            return;
         }
-
-        public int getCurrentRadioStation() {
-                return currentRadioStation;
+        if (newCurrentRadioStation < 0) {
+            return;
         }
+        currentRadioStation = newCurrentRadioStation;
+    }
 
-        public void setCurrentRadioStation(int currentRadioStation) {
-
-                if (currentRadioStation > maxRadioStation) {
-                        currentRadioStation = minRadioStation;
-                }
-                if (currentRadioStation < minRadioStation) {
-                        currentRadioStation = maxRadioStation;
-                }
-                this.currentRadioStation = currentRadioStation;
-
-        }
-
-        public void setCurrentVolume(int currentVolume) {
-
-                if (currentVolume > maxVolume) {
-                        currentVolume = minVolume;
-                }
-                if (currentVolume < minVolume) {
-                        currentVolume = maxVolume;
-                }
-                this.currentVolume = currentVolume;
-        }
+    public void setCurrentVolume(int newCurrentVolume) {
+//
+        currentVolume = newCurrentVolume;
+    }
 
 
-        public void pressNextRadioStation() {
-                if (currentRadioStation >= maxRadioStation) {
-                        setCurrentRadioStation(minRadioStation);
-                } else {
-                        setCurrentRadioStation(currentRadioStation + 1);
-                }
-        }
-
-        public void pressPrevRadioStation() {
-                if (currentRadioStation <= minRadioStation) {
-                        setCurrentRadioStation(maxRadioStation);
-                } else {
-                        setCurrentRadioStation(currentRadioStation - 1);
-                }
-        }
-
-        public int getMaxRadioStation() {
-                return maxRadioStation;
-        }
-
-        public void setMaxRadioStation(int maxRadioStation){
-            this.maxRadioStation = maxRadioStation;
-        }
-
-        public int getMinRadioStation() {
-            return minRadioStation;
-        }
-
-        public void setMinRadioStation( int minRadioStation){
-            this.minRadioStation = minRadioStation;
-        }
-
-        public int getCurrentVolume() {
-            return currentVolume;
-        }
-
-        public int getMaxVolume () {
-            return maxVolume;
-        }
-
-        public void setMaxVolume ( int maxVolume){
-            this.maxVolume = maxVolume;
-        }
-
-        public int getMinVolume () {
-            return minVolume;
-        }
-
-        public void setMinVolume ( int minVolume){
-            this.minVolume = minVolume;
-        }
-
-        public void pressVolumeUp () {
-            setCurrentVolume(currentVolume + 1);
-
-        }
-
-        public void pressVolumeDown () {
-            setCurrentVolume(currentVolume - 1);
+    public void volumeUp() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
         }
     }
+
+    public void volumeDown() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+
+    public void next() {
+        if (currentRadioStation < 9) {
+            currentRadioStation = currentRadioStation + 1;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentRadioStation > 0) {
+            currentRadioStation = currentRadioStation - 1;
+        } else {
+            currentRadioStation = 9;
+        }
+    }
+
+}
+
